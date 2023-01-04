618ff7d09290e14633b32afd53379adc34333c3b
praveen <kurukuripraveen@gmail.com>
2023-01-04 09:42:37 +0000

    came errors

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import characterCounter from './components/characterCounter'
+
+import './App.css'
+
+// Replace your code here
+const App = () => <characterCounter />
+
+export default App
---
+++ src/components/characterCounter/index.js
@@ -0,0 +1,10 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class characterCounter extends Component {
+  render() {
+    return <h1>praveen</h1>
+  }
+}
+export default characterCounter
